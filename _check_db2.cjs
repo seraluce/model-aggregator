9adaeb6264b4c123b43ae6b3431c512f070c866a
seraluce <seraluce@163.com>
2026-07-09 05:50:24 +0800

    cleanup: remove unused scripts

--- _check_db2.cjs
+++
@@ -1,7 +0,0 @@
-const Database = require('better-sqlite3');
-const db = new Database('server/data/freeapi.db');
-const tables = db.prepare("SELECT name FROM sqlite_master WHERE type='table'").all();
-console.log('Tables:', JSON.stringify(tables.map(t => t.name)));
-const keys = db.prepare('SELECT * FROM api_keys').all();
-console.log('api_keys:', JSON.stringify(keys));
-db.close();
--- _check_profile3.cjs
+++
@@ -1,9 +0,0 @@
-const Database = require('better-sqlite3');
-const db = new Database('server/data/freeapi.db');
-const pp = db.prepare("SELECT * FROM profiles WHERE name = 'Default'").all();
-if (pp.length > 0) {
-  const pm = db.prepare("SELECT m.model_id, m.platform, m.display_name, m.supports_vision, m.id, pm.priority FROM profile_models pm JOIN models m ON pm.model_db_id = m.id WHERE pm.profile_id = ? ORDER BY pm.priority").all(pp[0].id);
-  console.log('Models in Default profile:');
-  pm.forEach(m => console.log(`  [${m.platform}] ${m.model_id} prio=${m.priority} vision=${m.supports_vision}`));
-}
-db.close();
